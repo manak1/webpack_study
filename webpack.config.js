600171a606c075d832c92a967f8acf458a255aca
manak1 <mana23happy@hotmail.co.jp>
2020-06-12 01:42:59 +0900

    chore: add styleload in webapck setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
-const path = require('path')
+const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-      //resolveメソッドで絶対パスを取得している。
-      path:path.resolve(__dirname,"./dist"),
-      filename:'main.js'
-  }
-}
+    //resolveメソッドで絶対パスを取得している。
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        //cssファイル検知に使用 loader端から上の順に適応される。
+        test: /\.css/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
