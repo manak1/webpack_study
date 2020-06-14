66f03c97a8459ed5bd2e1b7370aaa3349e6142ca
manak1 <mana23happy@hotmail.co.jp>
2020-06-14 20:25:09 +0900

    chore: set targe browsers to transplie

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
 import '../css/style.scss'
 
+const obj = {a:1,b:2};
+
+const newObj = {...obj,c:3}
+
+console.log(newObj)
+
 const my = ()=> {
   console.log("is it running?")
 }
+
+
+
 my()
--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
           {
             loader: "babel-loader",
             options: {
-              presets: ["@babel/preset-env"],
+              presets: [["@babel/preset-env",{'targets':'> 0.25%, not dead'}]],
             },
           },
         ],
